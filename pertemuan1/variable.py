e14b00200c63a377e47f9a46348fc3b4a0e054b0
Apriyanto <114456673+Apryanto01@users.noreply.github.com>
2022-12-27 11:19:35 +0700

    upload

---
+++ pertemuan1/variable.py
@@ -0,0 +1,16 @@
+# Tipe data di python
+# 1. string
+# 2. Boolean
+# 3. Float
+# 4. Integer
+# 5. Hexadecimal
+# 6. Dll.
+
+nama = "Apri" # string
+umur = 21 # integer
+isStuden = True # boolean
+ip = 3.50 # float
+
+ang = Umur21thnmendatang = umur + 21 # operasi aritmatika hanya dapat dilakukan dengan ini
+
+print("Nama saya", nama, '. Umur saya 21 tahun mendatang adalah', Umur21thnmendatang)
